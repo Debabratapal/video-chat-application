9ad27236045be9fcb5c7960524317930bf936236
unknown <sangeet_joy@yahoo.com>
2020-05-20 23:57:02 +0530

    peer

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -6,6 +6,8 @@ const App: React.FC = (props) => {
   let remoteRef = React.createRef<HTMLVideoElement>();
   let textRef = React.createRef<HTMLTextAreaElement>();
 
+  const [text, setText] = React.useState("");
+
   const pc_config = {
     iceServers: [
       {
@@ -55,14 +57,36 @@ const App: React.FC = (props) => {
     window.navigator.getUserMedia(options, success, onError);
   }, []);
 
-  // const handleCreateOffer = () => {
-  //   pc.createOffer({ offerToReceiveVideo: 1 }).then(
-  //     (sdp) => {
-  //       pc.setLocalDescription(sdp);
-  //     },
-  //     (e) => {}
-  //   );
-  // };
+  const handleCreateOffer = () => {
+    pc.createOffer({ offerToReceiveVideo: true }).then(
+      (sdp) => {
+        console.log(JSON.stringify(sdp));
+        pc.setLocalDescription(sdp);
+      },
+      (e) => {}
+    );
+  };
+
+  const setRemoteDescription = () => {
+    const desc = JSON.parse(text);
+    pc.setRemoteDescription(new RTCSessionDescription(desc));
+  };
+
+  const createAnswer = () => {
+    pc.createAnswer({ offerToReceiveVideo: true }).then(
+      (sdp) => {
+        console.log(JSON.stringify(sdp));
+        pc.setLocalDescription(sdp);
+      },
+      (e) => {}
+    );
+  };
+
+  const addCandidate = () => {
+    const candidate = JSON.parse(text);
+    console.log("adding Candidate", candidate);
+    pc.addIceCandidate(new RTCIceCandidate(candidate));
+  };
 
   return (
     <>
@@ -70,9 +94,16 @@ const App: React.FC = (props) => {
         <video className="video-element" ref={videoRef} autoPlay />
         <video className="video-element" ref={remoteRef} autoPlay />
       </div>
-      <textarea ref={textRef}></textarea>
-      {/* <button onClick={handleCreateOffer}>Offer</button>
-      <button onClick={handleCreateAnswer}>Answer</button> */}
+      <button onClick={handleCreateOffer}>Offer</button>
+      <button onClick={createAnswer}>Answer</button>
+      <br />
+      <textarea
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      ></textarea>
+      <br />
+      <button onClick={setRemoteDescription}>Set Remote Description</button>
+      <button onClick={addCandidate}>Add Candidate</button>
     </>
   );
 };
